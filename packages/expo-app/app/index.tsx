a10dc1cdb07fbcdd9caabe1ac1c98612812dc200
has-1997 <scammaps@protonmail.com>
2025-07-06 19:48:03 +0100

    feat: scaffold react native app with expo

---
+++ packages/expo-app/app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello from my first app!</Text>
+    </View>
+  );
+}
